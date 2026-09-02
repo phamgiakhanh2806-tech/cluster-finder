c28e06e56dca0d98f6e70f843e3084de758d9152
phamgiakhanh2806-tech <phamgiakhanh2806@gmail.com>
2026-09-02 11:29:14 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
         name = "meteor-maven"
         url = uri("https://maven.meteordev.org/releases")
     }
+
     maven {
         name = "meteor-maven-snapshots"
         url = uri("https://maven.meteordev.org/snapshots")
@@ -23,49 +24,31 @@ repositories {
 }
 
 dependencies {
-    // Fabric
     minecraft(libs.minecraft)
-    implementation(libs.fabric.loader)
+    mappings(loom.officialMojangMappings())
 
-    // Meteor
-    implementation(libs.meteor.client)
+    modImplementation(libs.fabric.loader)
+    modImplementation(libs.meteor.client)
 }
 
+java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(libs.versions.jdk.get().toInt()))
+        languageVersion.set(
+            JavaLanguageVersion.of(libs.versions.jdk.get().toInt())
+        )
     }
-}
-
-fun toMinecraftCompat(version: String): String {
-    val stable = Regex("""^(\d{2})\.([1-9]\d*)(?:\.(\d+))?$""")
-
-    stable.matchEntire(version)?.let {
-        val (year, drop, _) = it.destructured
-        return "~$year.$drop"
-    }
-
-    val pre = Regex("""^(\d{2})\.([1-9]\d*)-pre[-.](\d+)$""")
-    pre.matchEntire(version)?.let {
-        return version.replace("-pre-", "-pre.")
-    }
-
-    val rc = Regex("""^(\d{2})\.([1-9]\d*)-rc[-.](\d+)$""")
-    rc.matchEntire(version)?.let {
-        return version.replace("-rc-", "-rc.")
-    }
-
-    return version
 }
 
 tasks {
     processResources {
         val propertyMap = mapOf(
             "version" to project.version,
-            "minecraft_version" to toMinecraftCompat(libs.versions.minecraft.get()),
-            "jdk_version" to libs.versions.jdk.get(),
+            "minecraft_version" to libs.versions.minecraft.get(),
+            "jdk_version" to libs.versions.jdk.get()
         )
 
         inputs.properties(propertyMap)
+
         filesMatching("fabric.mod.json") {
             expand(propertyMap)
         }
@@ -75,7 +58,9 @@ tasks {
         inputs.property("archivesName", archivesBaseName)
 
         from("LICENSE") {
-            rename { "${it}_$archivesBaseName" }
+            rename {
+                "${it}_${archivesBaseName}"
+            }
         }
     }
 
